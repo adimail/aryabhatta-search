265979d3136ed67733580c254a30a89010e6edd7
Shreya Kulkarni <shreya.kulkarni132@gmail.com>
2025-02-22 16:32:51 +0530

    sidebar on nav

--- src/components/common/nav.tsx
+++ src/components/common/nav.tsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
 import { auth } from "@/server/auth";
 import { AuthButtons } from "./auth-buttons";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { IoMenu } from "react-icons/io5";
 
 export const Nav = async () => {
   const session = await auth();
@@ -8,6 +17,21 @@ export const Nav = async () => {
   return (
     <header className="fixed left-0 right-0 top-0 z-50 bg-white shadow-md">
       <div className="container mx-auto flex items-center justify-between px-3 py-4 md:py-6">
+        <Sheet>
+          <SheetTrigger>
+            <IoMenu size={30} />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+
         <Link href="/" className="flex items-center">
           <h1 className="text-lg font-bold md:text-2xl">Aryabhatta Search</h1>
         </Link>
